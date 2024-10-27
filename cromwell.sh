7a610be3905407f7e1599d5b21750c6888ca904d
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2024-10-27 16:46:42 +0800

    Add run file

---
+++ cromwell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#load modules
+module load singularity/4.1.0-slurm
+
+#Define variables
+wdl=/software/projects/pawsey0933/giraffe/wdl/giraffe_and_deepvariant_slurm_2024-10-26.wdl
+config=/software/projects/pawsey0933/cromwell/cromwell.conf
+inputs=/software/projects/pawsey0933/giraffe/wdl/giraffe_and_deepvariant.json
+jar=/software/projects/pawsey0933/cromwell/cromwell-87.jar
+
+#change to working dir
+cd /scratch/pawsey0933/cfolland/giraffe
+rm -rf /scratch/pawsey0933/cfolland/giraffe/cromwell-executions
+rm -rf /scratch/pawsey0933/cfolland/giraffe/cromwell-workflow-logs
+rm -rf /scratch/pawsey0933/cfolland/giraffe/singularity-cache
+
+# Run the Cromwell workflow with the specified configuration file
+java -Dconfig.file=$config -jar $jar run $wdl --inputs $inputs
