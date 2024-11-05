8f6c39d945ee06a6fc88bbb89861f09ae1e5e6a2
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2024-11-05 20:56:16 +0800

    Update cromwell.sh

--- cromwell.sh
+++ cromwell.sh
@@ -4,7 +4,7 @@
 module load singularity/4.1.0-slurm
 
 #Define variables
-wdl=/software/projects/pawsey0933/giraffe/wdl/giraffe_and_deepvariant_slurm_2024-10-26.wdl
+wdl=/software/projects/pawsey0933/giraffe/wdl/giraffe_and_deepvariant_slurm.wdl
 config=/software/projects/pawsey0933/cromwell/cromwell.conf
 inputs=/software/projects/pawsey0933/giraffe/wdl/giraffe_and_deepvariant.json
 jar=/software/projects/pawsey0933/cromwell/cromwell-87.jar
