66735b698594db424da1f3ef36e36e3f6b7285aa
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2024-11-27 16:16:19 +0800

    Create download.sh

---
+++ snakemake/download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=download_pangenome
+#SBATCH --account=pawsey0933
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --time=3:00:00
+#SBATCH --mail-user=chiara.folland@perkins.org.au
+#SBATCH --mail-type=END
+#SBATCH --error=%j.%x.err
+#SBATCH --output=%j.%x.out
+#SBATCH --export=ALL
+
+cd /scratch/pawsey0933/cfolland/vg_snakemake/refs
+
+wget https://s3-us-west-2.amazonaws.com/human-pangenomics/pangenomes/freeze/freeze1/minigraph-cactus/hprc-v1.1-mc-grch38/hprc-v1.1-mc-grch38.gbz
+wget https://s3-us-west-2.amazonaws.com/human-pangenomics/pangenomes/freeze/freeze1/minigraph-cactus/hprc-v1.1-mc-grch38/hprc-v1.1-mc-grch38.hapl
